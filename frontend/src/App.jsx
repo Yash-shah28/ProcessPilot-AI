1f35fa6c2c4b14bc56852ff8de1fb074a4eec18b
Yash-shah28 <yashshah28072004@gmail.com>
2025-06-27 10:33:28 +0530

    User Authentication done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,90 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import FloatingShape from "./components/FloatingShape";
+
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import LoadingSpinner from "./components/LoadingSpinner";
+
+import { Toaster } from "react-hot-toast";
+import { useContext, useEffect } from "react";
+import { UserContext } from "./Context/UserContext";
+
+// protect routes that require authentication
+const ProtectedRoute = ({ children }) => {
+	const { userAuth } = useContext(UserContext);
+
+	if (!userAuth.isAuthenticated) {
+		return <Navigate to='/login' replace />;
+	}
+
+
+	return children;
+};
+
+// redirect authenticated users to the home page
+const RedirectAuthenticatedUser = ({ children }) => {
+	const { userAuth } = useContext(UserContext);
+
+	if (userAuth.isAuthenticated ) {
+		return <Navigate to='/' replace />;
+	}
+
+	return children;
+};
+
+function App() {
+	const { userAuth, checkAuth } = useContext(UserContext);
+
+	useEffect(() => {
+		checkAuth();
+	}, []);
+
+	if (userAuth.isCheckingAuth) return <LoadingSpinner />;
+
+	return (
+		<div
+			className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'
+		>
+			<FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<ProtectedRoute>
+							<DashboardPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path='/signup'
+					element={
+						<RedirectAuthenticatedUser>
+							<SignUpPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				<Route
+					path='/login'
+					element={
+						<RedirectAuthenticatedUser>
+							<LoginPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				
+
+				
+				{/* catch all routes */}
+				<Route path='*' element={<Navigate to='/' replace />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
+}
+
+export default App;
---
+++ frontend/src/Context/UserContext.jsx
@@ -0,0 +1,118 @@
+
+import axios from "axios";
+import { createContext, useState } from "react";
+
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+
+axios.defaults.withCredentials = true;
+
+export const UserContext = createContext();
+
+export const UserContextProvider = ({ children }) => {
+
+	const [userAuth, setUserAuth] = useState({
+		user: null,
+		isAuthenticated: false,
+		error: null,
+		isLoading: false,
+		isCheckingAuth: true,
+		message: null,
+	})
+
+
+	const signup = async (email, password, name) => {
+		setUserAuth(prev => ({ ...prev, isLoading: true, error: null }));
+		try {
+			const response = await axios.post(`${API_URL}/signup`, { email, password, name });
+			setUserAuth(prev => ({...prev, user: response.data.user, isAuthenticated: true, isLoading: false }));
+		} catch (error) {
+			setUserAuth(prev => ({...prev, error: error.response.data.message || "Error signing up", isLoading: false }));
+			throw error;
+		}
+	};
+
+	const login = async (email, password) => {
+		console.log(email,password)
+		setUserAuth(prev => ({ ...prev, isLoading: true, error: null }));
+		try {
+			const response = await axios.post(`${API_URL}/login`, { email, password });
+			setUserAuth(prev =>({
+				...prev,
+				isAuthenticated: true,
+				user: response.data.user,
+				error: null,
+				isLoading: false,
+			}));
+		} catch (error) {
+			setUserAuth(prev =>({ ...prev, error: error.response?.data?.message || "Error logging in", isLoading: false }));
+			throw error;
+		}
+	};
+
+	const logout = async () => {
+		setUserAuth(prev => ({ ...prev, isLoading: true, error: null }));
+		try {
+			await axios.post(`${API_URL}/logout`);
+			setUserAuth(prev => ({...prev, user: null, isAuthenticated: false, error: null, isLoading: false }));
+		} catch (error) {
+			setUserAuth(prev =>({ ...prev, error: "Error logging out", isLoading: false }));
+			throw error;
+		}
+	};
+	const verifyEmail = async (code) => {
+		setUserAuth(prev => ({ ...prev, isLoading: true, error: null }));
+		try {
+			const response = await axios.post(`${API_URL}/verify-email`, { code });
+			setUserAuth(prev =>({ ...prev, user: response.data.user, isAuthenticated: true, isLoading: false }));
+			return response.data;
+		} catch (error) {
+			setUserAuth(prev => ({...prev, error: error.response.data.message || "Error verifying email", isLoading: false }));
+			throw error;
+		}
+	};
+	const checkAuth = async () => {
+		setUserAuth(prev => ({ ...prev, isCheckingAuth: true, error: null }));
+		try {
+			const response = await axios.get(`${API_URL}/check-auth`);
+			setUserAuth(prev =>({...prev, user: response.data.user, isAuthenticated: true, isCheckingAuth: false }));
+		} catch (error) {
+			setUserAuth(prev  =>({...prev, error: null, isCheckingAuth: false, isAuthenticated: false }));
+		}
+	};
+	const forgotPassword = async (email) => {
+		setUserAuth(prev => ({ ...prev, isLoading: true, error: null }));
+		try {
+			const response = await axios.post(`${API_URL}/forgot-password`, { email });
+			setUserAuth(prev => ({...prev, message: response.data.message, isLoading: false }));
+		} catch (error) {
+			setUserAuth(prev =>({
+				...prev,
+				isLoading: false,
+				error: error.response.data.message || "Error sending reset password email",
+			}));
+			throw error;
+		}
+	};
+	const resetPassword = async (token, password) => {
+		setUserAuth(prev => ({ ...prev, isLoading: true, error: null }));
+		try {
+			const response = await axios.post(`${API_URL}/reset-password/${token}`, { password });
+			setUserAuth(prev => ({...prev, message: response.data.message, isLoading: false }));
+		} catch (error) {
+			setUserAuth(prev => ({
+				...prev,
+				isLoading: false,
+				error: error.response.data.message || "Error resetting password",
+			}));
+			throw error;
+		}
+	};
+
+	return (
+		<UserContext.Provider value={{ userAuth, setUserAuth, signup, login, logout, verifyEmail, forgotPassword, resetPassword, checkAuth }}>
+			{children}
+		</UserContext.Provider>
+	)
+};
+
+export default UserContextProvider;
